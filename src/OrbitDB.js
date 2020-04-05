ea68e610dabf5c8df9db2d5cd04b15a108047f54
phillmac <phillmac@users.noreply.github.com>
2020-04-05 12:13:59 +1000

    Debug load event

--- src/OrbitDB.js
+++ src/OrbitDB.js
@@ -176,9 +176,9 @@ class OrbitDB {
     }
 
     // Remove event listeners
-    this.events.removeAllListeners('open')
-    this.events.removeAllListeners('load')
-    this.events.removeAllListeners('ready')
+    this.events.removeAllListeners('orbitdb.open')
+    this.events.removeAllListeners('orbitdb.load')
+    this.events.removeAllListeners('orbitdb.ready')
 
     const caches = Object.keys(this.caches)
     for (const directory of caches) {
@@ -334,7 +334,7 @@ class OrbitDB {
     const dir = db && db.options.directory ? db.options.directory : this.directory
     await this._requestCache(address, dir, db._cache)
     this.stores[address] = db
-    this.events.emit('load', address)
+    this.events.emit('orbitdb.load', address)
   }
 
   async _determineAddress (name, type, options = {}) {
@@ -433,8 +433,8 @@ class OrbitDB {
         options.overwrite = options.overwrite ? options.overwrite : true
         const events = this.events
         return this.create(address, options.type, options).then((db) => {
-          db.events.on('ready', (address, heads) => events.emit('ready', address, heads))
-          events.emit('open', db.address.toString())
+          db.events.on('orbitdb.ready', (address, heads) => events.emit('ready', address, heads))
+          events.emit('orbitdb.open', db.address.toString())
           return db
         })
       }
