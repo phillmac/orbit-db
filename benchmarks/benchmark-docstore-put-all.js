5fb80f59a95accb850893a0214d753af13158ea1
phillmac <phillmac@users.noreply.github.com>
2020-04-12 20:00:13 +1000

    Add benchmark docstore put all

---
+++ benchmarks/benchmark-docstore-put-all.js
@@ -0,0 +1,83 @@
+'use strict'
+
+const IPFS = require('ipfs')
+const IPFSRepo = require('ipfs-repo')
+const DatastoreLevel = require('datastore-level')
+const OrbitDB = require('../src/OrbitDB')
+
+// Metrics
+let totalQueries = 0
+let seconds = 0
+let queriesPerSecond = 0
+let lastTenSeconds = 0
+
+
+items = []
+
+for(let i=0; i<100000;i++){
+  console.log(`Generating set ${i}`)
+  items[i] = []
+  for(let i2=0; i2<1000;i2++){
+    items[i][i2] = {_id: i*1000 + i2, data: Math.random().toString(36).substring(5)}
+  }
+}
+
+// Main loop
+const queryLoop = async (db) => {
+  await db.putAll(items[totalQueries],  { pin: false })
+  totalQueries ++
+  lastTenSeconds ++
+  queriesPerSecond ++
+  setImmediate(() => queryLoop(db))
+}
+
+// Start
+console.log("Starting IPFS daemon...")
+
+const repoConf = {
+  storageBackends: {
+    blocks: DatastoreLevel,
+  },
+}
+
+const ipfs = new IPFS({
+  repo: new IPFSRepo('./orbitdb/benchmarks/ipfs', repoConf),
+  start: false,
+  EXPERIMENTAL: {
+    sharding: false,
+    dht: false,
+  },
+})
+
+ipfs.on('error', (err) => console.error(err))
+
+ipfs.on('ready', async () => {
+  const run = async () => {
+    try {
+      const orbit = await OrbitDB.createInstance(ipfs,{ directory: './orbitdb/benchmarks' })
+      const db = await orbit.docstore('orbit-db.benchmark', {
+        replicate: false,
+      })
+
+      // Metrics output
+      setInterval(() => {
+        seconds ++
+        if(seconds % 10 === 0) {
+          console.log(`--> Average of ${lastTenSeconds/10} q/s in the last 10 seconds`)
+          if(lastTenSeconds === 0)
+            throw new Error("Problems!")
+          lastTenSeconds = 0
+        }
+        console.log(`${queriesPerSecond} queries per second, ${totalQueries} queries in ${seconds} seconds (Oplog: ${db._oplog.length})`)
+        queriesPerSecond = 0
+      }, 1000)
+      // Start the main loop
+      queryLoop(db)
+    } catch (e) {
+      console.log(e)
+      process.exit(1)
+    }
+  }
+
+  run()
+})
