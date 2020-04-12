6d113ddb132d1134137d5e69cbf4e8d0db3652ea
phillmac <phillmac@users.noreply.github.com>
2020-04-12 20:12:25 +1000

    Start id from 1

--- benchmarks/benchmark-docstore-put-all.js
+++ benchmarks/benchmark-docstore-put-all.js
@@ -17,8 +17,8 @@ const items = []
 for(let i=0; i<10000;i++){
   console.log(`Generating set ${i}`)
   items[i] = []
-  for(let i2=0; i2<1000;i2++){
-    items[i][i2] = {_id: i*1000 + i2, data: Math.random().toString(36).substring(5)}
+  for(let i2=1; i2<1000;i2++){
+    items[i][i2] = {_id:i*1000 + i2, data: Math.random().toString(36).substring(5)}
   }
 }
 
